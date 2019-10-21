4cd518c3c2e254bc94a11a9ddd7ca18630c915a8
Robbie Gay <robbiecgay@gmail.com>
2019-10-21 11:50:03 -0400

    Got fetch to work

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React from 'react';
 import Layout from './Layout.js';
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
 
   
-}
+// }
 
 function App() {
   return (
-    <Layout menuData={} />
+    <Layout /> //menuData={} 
   );
 }
 
--- src/Menu.js
+++ src/Menu.js
@@ -1,43 +1,40 @@
 import React from 'react';
 
-
-// Model
-// function getMenuItems(x) {
-//     fetch('https://entree-f18.herokuapp.com/v1/menu/12').then(function (response) {
-//         response.json().then(function (data) {
-//             console.log(data.menu_items[x].description);
-//             // return data.menu_items[x].description;
-//             // SET STATE
-//             this.setState({item0:data.menu_items});
-//         });
-//     });
-// }
-
 class Menu extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {item0: null};
+        this.state = { menuItems: 'Generic Food' };
+    }
+
+    getMenuItems() {
+        // 'this' can't be accessed in fetch, so I pass it in as a variable
+        const that = this;
+        fetch('https://entree-f18.herokuapp.com/v1/menu/12').then(function (response) {
+            response.json().then(function (data) {
+                that.setState({ menuItems: data.menu_items });
+            });
+        });
+
     }
 
     // Controller
     componentDidMount() {
-        // this.setState({item0: getMenuItems(0)});
-        getMenuItems(0);
+        this.getMenuItems();
     }
 
     componentWillUnmount() {
 
     }
 
+    // Lets me generate random prices, in a low, medium, high range
     setPrice(x) {
-        let priceRange = [5, 10, 15]
-        let y = Math.random() * priceRange[x];
-        return `$${Math.ceil(Math.random() * y)}`;
+        const priceRange = [5, 10, 15]
+        return `$${Math.ceil(Math.random() * 5) + priceRange[x]}`;
     }
 
     render() {
         return (
-            <React.Fragment>
+            <React.Fragment >
                 <ul className="nav nav-pills nav-justified mb-3" id="pills-tab" role="tablist">
                     <li className="nav-item">
                         <a className="nav-link active" id="apps-tab" data-toggle="pill" href="#pills-apps" role="tab" aria-controls="pills-apps" aria-selected="true">Appetizers</a>
@@ -56,15 +53,52 @@ class Menu extends React.Component {
                     </li>
                 </ul>
                 <div className="tab-content" id="pills-tabContent">
-                    <div className="tab-pane fade show active" id="pills-apps" role="tabpanel" aria-labelledby="apps-tab">{this.state.item0}</div>
-                    <div className="tab-pane fade" id="pills-lunch" role="tabpanel" aria-labelledby="lunch-tab">Lunch iteams {this.setPrice(1)}</div>
-                    <div className="tab-pane fade" id="pills-snacks" role="tabpanel" aria-labelledby="snacks-tab">Snacks — lots of them</div>
-                    <div className="tab-pane fade" id="pills-dinner" role="tabpanel" aria-labelledby="dinner-tab">Dinner. Yum.</div>
-                    <div className="tab-pane fade" id="pills-secondDinner" role="tabpanel" aria-labelledby="secondDinner-tab">Still hungry?</div>
+                    <div className="tab-pane fade show active" id="pills-apps" role="tabpanel" aria-labelledby="apps-tab">{this.state.menuItems[0].description} {this.setPrice(0)}</div>
+                    <div className="tab-pane fade" id="pills-lunch" role="tabpanel" aria-labelledby="lunch-tab">Lunch items {this.setPrice(0)}</div>
+                    <div className="tab-pane fade" id="pills-snacks" role="tabpanel" aria-labelledby="snacks-tab">Snacks — lots of them {this.setPrice(1)}</div>
+                    <div className="tab-pane fade" id="pills-dinner" role="tabpanel" aria-labelledby="dinner-tab">Dinner. Yum. {this.setPrice(2)}</div>
+                    <div className="tab-pane fade" id="pills-secondDinner" role="tabpanel" aria-labelledby="secondDinner-tab">Still hungry? {this.setPrice(1)}</div>
+                    {console.log(this.state)}
                 </div>
-            </React.Fragment>
+            </React.Fragment >
         );
     }
 }
 
 export default Menu;
+
+
+
+
+
+// Model
+// function getMenuItems() {
+//     fetch('https://entree-f18.herokuapp.com/v1/menu/12').then(function (response) {
+//         response.json().then(function (data) {
+//             // SET STATE
+//             // this.setState({ menuItems: data.menu_items });
+//             this.setState({ menuItems: 'Test Food' });
+//             // console.log(data.menu_items);
+//         });
+//     });
+// }
+
+// async getMenuItems() {
+//     this.setState({ menuItems: 'eruireuireuiertui' });
+//     let x = await fetch('https://entree-f18.herokuapp.com/v1/menu/12').then(function (response) {
+//         response.json().then(function (data) {
+//             console.log(data.menu_items);
+//             return data.menu_items;
+//         });
+//     });
+//     console.log(`x = ${x}`);
+//     this.setState({ menuItems: x });
+// }
+
+// fetchAPI() {
+//     fetch('https://entree-f18.herokuapp.com/v1/menu/12').then(function (response) {
+//         response.json().then(function (data) {
+//             return data.menu_items;
+//         });
+//     });
+// }
