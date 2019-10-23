f9b5b95d1910998015b5f065eaea84754decc482
Robbie Gay <robbiecgay@gmail.com>
2019-10-23 14:51:00 -0400

    Updates

---
+++ precache-manifest.0fcb5b5a8175c79bd7b5ff15ddf3744f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d694c82aa4984afbf5a184f224ef3ff9",
+    "url": "/react-restaurant/index.html"
+  },
+  {
+    "revision": "9babbbffcbed21a3d3bb",
+    "url": "/react-restaurant/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "cd6e47baf8c7e655e333",
+    "url": "/react-restaurant/static/css/main.558f1c92.chunk.css"
+  },
+  {
+    "revision": "9babbbffcbed21a3d3bb",
+    "url": "/react-restaurant/static/js/2.acbee034.chunk.js"
+  },
+  {
+    "revision": "cd6e47baf8c7e655e333",
+    "url": "/react-restaurant/static/js/main.c3adc430.chunk.js"
+  },
+  {
+    "revision": "bfe872071d8e7cef72c0",
+    "url": "/react-restaurant/static/js/runtime-main.eb30f3de.js"
+  }
+]);
